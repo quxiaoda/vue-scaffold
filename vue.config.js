7e2812621d664277d3ec5556d954731d982f22af
quda <quda@rd.tuan800.com>
2018-04-23 17:36:16 +0800

    init project

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+/**
+ * 自定义配置
+ * 详细配置参考：https://github.com/vuejs/vue-cli/blob/dev/docs/config.md
+ * proxy配置： https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+ */
+module.exports = {
+    devServer: {
+    //   port: 8989
+    }
+  }
