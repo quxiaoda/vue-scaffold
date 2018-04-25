ec46c0e8259b0be007ac7a3ea5cfbddc007ded21
quda <quda@baidu.com>
2018-04-25 21:20:50 +0800

    stage 1

---
+++ src/components/Box.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="box">
+      <span class="content">{{ content }}</span>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+    .box {
+        width: 100px;
+        height: 100px;
+        line-height: 100px;
+        text-align: center;
+        background: #cccccc;
+        .content {
+            font-size: 10px;
+            color: #ffffff;
+        }
+    }
+</style>
+
+<script lang="ts">
+import {Component, Vue, Prop} from 'vue-property-decorator';
+
+@Component
+export default class Box extends Vue {
+     // undecorated property will be packed in `data` option
+    public defaultContent = 'Hello';
+
+    @Prop({default: 'World'})
+    public msg: string;
+
+    // Computed properties are written as getters and setters on the class.
+    get content() {
+        return this.defaultContent + ' ' + this.msg;
+    }
+}
+</script>
+
+
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <box :msg="msg"></box>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Box from '@/components/Box.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    Box,
+  },
+})
+export default class About extends Vue {
+  public msg: string = 'quda!';
+}
+</script>
